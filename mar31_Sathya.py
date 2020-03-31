4d09a3274b83ffdf1aa48c06534fce92620707bf
Sathya2020 <62892486+Sathya2020@users.noreply.github.com>
2020-03-31 22:44:27 +0530

    Add files via upload

--- mar31_Sathya.py
+++ mar31_Sathya.py
@@ -1,6 +1,67 @@
+'''
 r=int(input("enter the value of r: "))
 pie=22/7
 area=pie*r**2
 AREA=int(area)
 print(r)
 print("area of circle: ",AREA)
+'''
+'''
+# a**2 - b**2
+a=int(input("enter the value of a: "))
+b=int(input("enter the value of b: "))
+print(a)
+print(b)
+c=(a**2) - (b**2)
+print(c)
+
+a=3
+b=4
+c=(a**2) - (b**2)
+print(c)
+'''
+'''
+a=float(input("enter the value of a: "))
+b=float(input("enter the value of b: "))
+c=a+b
+print(a)
+print(b)
+print(c)
+d=int(c)
+print(d)
+'''
+'''
+a=str(input("enter the value of a: "))
+b=str(input("enter the value of b: "))
+print(a)
+print(b)
+c=a+b
+print(c)
+'''
+'''
+a=5
+b=3
+c=a-b
+print(c)
+'''
+'''
+a=8
+b=9
+c=(a+b)**2
+print(c)
+'''
+'''
+a=5
+b=3
+c=a**2 + b**2 + 2*a*b
+print(c)
+'''
+r=float(input("enter the value of radius: "))
+h=float(input("enter the value of height: "))
+pi=3.14
+area=pi*(r**2)*h
+print(r)
+print(h)
+print(area)
+A=int(area)
+print(A)
