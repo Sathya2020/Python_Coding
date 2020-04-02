3f500c113a88eb6ba2602233fd2cc9eda37a4ecc
Sathya2020 <62892486+Sathya2020@users.noreply.github.com>
2020-04-02 21:37:29 +0530

    Add files via upload

---
+++ APR1.2.py
@@ -0,0 +1,20 @@
+'''
+r=float(input("enter the value of r: "))
+pie=3.14
+#AREA OF CIRCLE
+area=pie*(r**2)
+print("area of circle with radius{0} is {1}".format(r,area))
+AREA=int(area)
+print(r)
+print(area)
+print(AREA)
+'''
+r=float(input("enter the value of r: "))
+pie=3.14
+circumference =2*pie*r
+C=int(circumference)
+print("circumference of circle with radius{0} is {1}" .format(r,circumference))
+print(C)
+
+
+        
---
+++ APR1.py
@@ -0,0 +1,19 @@
+'''
+l=float(input("enter the value of length: "))
+b=float(input("enter the value of breadth: "))
+perimeter=2*(l+b)
+print(l)
+print(b)
+print(perimeter)
+P=int(perimeter)
+print(P)
+'''
+r=int(input("enter radius: "))
+h=int(input("enter heigth: "))
+pie=3.14
+v=(1/3)*pie*(r**2)*h
+print(r)
+print(h)
+print("volume of cone with radius{0} and heigth{1} is {2}".format(r,h,v))
+        
+        
