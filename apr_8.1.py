a6ee43f08d1419e2ce4c946ef5eda26699c6a47f
Sathya2020 <62892486+Sathya2020@users.noreply.github.com>
2020-04-08 23:46:20 +0530

    Add files via upload

---
+++ apr_8.1.py
@@ -0,0 +1,13 @@
+'''
+a=[2,3,4,6,7,8,6,0,6]
+d=int(len(a)/2)
+print(d)
+print(a[d])
+'''
+b=["hello","welcome","python","math","science"]
+f=int(len(b)/2)
+print(b[f])
+g=b[f]
+h=int(len(g)/2)
+
+print(g[h])
