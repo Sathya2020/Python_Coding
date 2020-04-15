740010e4cee544f20affbd7287d5358368fcdacc
Sathya2020 <62892486+Sathya2020@users.noreply.github.com>
2020-04-15 15:57:24 +0530

    Add files via upload

---
+++ Apr_15.py
@@ -0,0 +1,37 @@
+#even numbers from user input to user input
+n=int(input(" enter the value of n:"))
+i=0
+while i<=n:
+    print(i)
+    i=i+2
+else:
+    print("finished")
+    
+# multiples of 5 from 100 to 200
+n=200
+i=20
+
+while i<=40:
+    mul=5*i
+    print(mul)
+    i=i+1
+else:
+    print("task finished")
+    
+#multiples of 8 from user input to user input
+n=int(input("enter the value :"))
+i=1
+while i<=10:
+    ans=i*8
+    print(ans)
+    i=i+1
+print("over")
+
+#even numbers from 0 to 20
+n=20
+i=0
+while i<=n:
+    print(i)
+    i=i+2
+else:
+    print("finished")
